f7ba502c03f2fb392fdef8c1daf71d4e39dec931
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-02-25 11:10:28 -0500

    Sync dist script with Vault

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # Get the version from the command line
@@ -8,22 +8,40 @@ if [ -z $VERSION ]; then
   exit 1
 fi
 
+# Make sure we have AWS API keys
+if ([ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ]) && [ ! -z $HC_RELEASE ]; then
+    echo "Please set your AWS access key information in the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env vars."
+    exit 1
+fi
+
+if [ -z $NOBUILD ] && [ -z $DOCKER_CROSS_IMAGE ]; then
+    echo "Please set the Docker cross-compile image in DOCKER_CROSS_IMAGE"
+    exit 1
+fi
+
 # Get the parent directory of where this script is.
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 
-# Get the name of the binary
-NAME="$(basename "$DIR")"
-
 # Change into that dir because we expect that
 cd $DIR
 
-# Generate the tag
+if [ -z $RELBRANCH ]; then
+  RELBRANCH=master
+fi
+
+# Tag, unless told not to
 if [ -z $NOTAG ]; then
   echo "==> Tagging..."
-  git commit --allow-empty -a --gpg-sign=348FFC4C -m "Release v$VERSION"
-  git tag -a -m "Version $VERSION" -s -u 348FFC4C "v${VERSION}" master
+  git commit --allow-empty -a --gpg-sign=348FFC4C -m "Cut version $VERSION"
+  git tag -a -m "Version $VERSION" -s -u 348FFC4C "v${VERSION}" $RELBRANCH
+fi
+
+# Build the packages
+if [ -z $NOBUILD ]; then
+  # This should be a local build of the Dockerfile in the cross dir
+  docker run --rm -v "$(pwd)":/gopath/src/github.com/hashicorp/vault-ssh-helper -w /gopath/src/github.com/hashicorp/vault-ssh-helper ${DOCKER_CROSS_IMAGE}
 fi
 
 # Zip all the files
@@ -34,11 +52,23 @@ for FILENAME in $(find ./pkg -mindepth 1 -maxdepth 1 -type f); do
   cp ./pkg/${FILENAME} ./pkg/dist/${NAME}_${VERSION}_${FILENAME}
 done
 
-# Make the checksums
-pushd ./pkg/dist
-shasum -a256 * > ./${NAME}_${VERSION}_SHA256SUMS
 if [ -z $NOSIGN ]; then
   echo "==> Signing..."
-  gpg --default-key 348FFC4C --detach-sig ./${NAME}_${VERSION}_SHA256SUMS
+  pushd ./pkg/dist
+  rm -f ./vault-ssh-helper_${VERSION}_SHA256SUMS*
+  shasum -a256 * > ./vault-ssh-helper_${VERSION}_SHA256SUMS
+  gpg --default-key 348FFC4C --detach-sig ./vault-ssh-helper_${VERSION}_SHA256SUMS
+  popd
 fi
-popd
+
+# Upload
+if [ ! -z $HC_RELEASE ]; then
+  hc-releases -upload $DIR/pkg/dist --publish --purge
+
+  for FILENAME in $(find $DIR/pkg/dist -type f); do
+    FILENAME=$(basename $FILENAME)
+    curl -X PURGE https://releases.hashicorp.com/vault-ssh-helper/${VERSION}/${FILENAME}
+  done
+fi
+
+exit 0
