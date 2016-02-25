fa05e51e012be9059c686f10f4eaa6c73c903f32
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-02-25 15:47:13 -0500

    Fix building of openbsd/freebsd and improve purging behavior

--- scripts/build.sh
+++ scripts/build.sh
@@ -47,8 +47,6 @@ fi
 echo "==> Building..."
 gox \
     -os="${XC_OS}" \
-    -os="!freebsd" \
-    -os="!openbsd" \
     -arch="${XC_ARCH}" \
     -ldflags "-X github.com/hashicorp/vault-ssh-helper/main.GitCommit ${GIT_COMMIT}${GIT_DIRTY}" \
     -output "pkg/{{.OS}}_{{.Arch}}/vault-ssh-helper" \
