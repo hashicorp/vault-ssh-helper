59634c3fd71505abe1766a1ec25567927833b589
vishalnayak <vishalnayakv@gmail.com>
2016-03-07 08:11:25 -0500

    helper test changes

--- helper/agent_test.go
+++ helper/agent_test.go
@@ -8,16 +8,27 @@ import (
 func TestBelongsToCIDR(t *testing.T) {
 	t.Parallel()
 	testIP := net.ParseIP("10.50.100.101")
-	boo, err := belongsToCIDR(testIP, "0.0.0.0/0")
-	if err != nil || !boo {
-		t.Error("belongsToCIDR test 1 failed")
+	testCIDR := "0.0.0.0/0"
+	belongs, err := belongsToCIDR(testIP, testCIDR)
+	if err != nil {
+		t.Fatalf("err: %s", err)
 	}
-	boo, err = belongsToCIDR(testIP, "192.168.0.1/16")
-	if err != nil || boo {
-		t.Error("belongsToCIDR test 2 failed")
+	if !belongs {
+		t.Fatalf("bad: expected:true, actual:false")
 	}
-	boo, err = belongsToCIDR(testIP, "failure")
+
+	testCIDR = "192.168.0.1/16"
+	belongs, err = belongsToCIDR(testIP, testCIDR)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if belongs {
+		t.Fatalf("bad: expected:false, actual:true")
+	}
+
+	testCIDR = "invalid"
+	_, err = belongsToCIDR(testIP, testCIDR)
 	if err == nil {
-		t.Error("belongsToCIDR test 3 failed")
+		t.Fatalf("expected error")
 	}
 }
