574dcaf5c42ed8deb315b365d3ef4011d2076e22
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2017-11-08 10:36:58 -0500

    Bump version

--- version.go
+++ version.go
@@ -8,7 +8,7 @@ import (
 var GitCommit string
 
 const Name = "vault-ssh-helper"
-const Version = "0.1.3"
+const Version = "0.1.4"
 const VersionPrerelease = ""
 
 // formattedVersion returns a formatted version string which includes the git
