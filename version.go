7c2b3d27b1e0365bc05b266db90c15132f54feea
Victor Rodriguez <vrizo@hashicorp.com>
2026-05-28 14:31:34 -0400

    Bump project version to 0.2.3. Update changelog.

--- version.go
+++ version.go
@@ -11,7 +11,7 @@ import (
 var GitCommit string
 
 const Name = "vault-ssh-helper"
-const Version = "0.2.1"
+const Version = "0.2.3"
 const VersionPrerelease = ""
 
 // formattedVersion returns a formatted version string which includes the git
